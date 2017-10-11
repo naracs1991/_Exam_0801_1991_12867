6eb971b402e1b55e86ac816d9b55239c5e2791a3
naracs1991 <naracs1991@yahoo.com>
2017-10-11 11:19:52 -0800

    Segundo Intento a la Pregunta No.16 del Examen
    Segundo Intento a la Pregunta No.16 del Examen

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-  router.get('/index',
-  function(req,res,next){
-      var NameAndInfo = Object.assign('Nelson Rivera','0801_1991_12867','naracs1991@gmail.com');
-      });
-      res.json(NameAndInfo);
-  }
- );
-//-------------------------------------------
-var numbers = {
-  firstnumber:10,
-  secondnumber:10,
+//------------------------------------------------------------------------------
+var ExamArray = [];
+var ExamOfPupilInfo ={
+  Name:"Nelson Rivera",AccoutNumber:"0801_1991_12867",Email:"naracs1991@gmail.com"
 }
 
- router.post('/SumOfTwoNumbers', function(req,res,next){
-   var firstnumber = Object.assign({},numbers,req.body);
-   var secondnumber = Object.assign({},numbers,req.body);
-   var result = (firstnumber+secondnumber);
-   res.json(result);
+var SumNumbers =[];
+var Sum={ FirstNumber:"",SecondNumber:"",Answer:""}
+
+  router.get("/AccoutNumber", function(req,res,next){
+    res.send(ExamOfPupilInfo);
   });
 
+  router.get('/SumOfTwoNumbers', function(req,res,next){
+    res.render('SumOfTwoNumbers',{});
+  });
+
+  router.post('/SumOfTwoNumbers', function(req,res,next){
+    console.log(req.body);
+    var DataRegistry = Object.assign({},Sum);
+        DataRegistry.FirstNumber = parseInt( req.body.txtFirstNumber);
+        DataRegistry.SecondNumber = parseInt(req.body.txtSecondNumber);
+        DataRegistry.Answer = DataRegistry.FirstNumber + DataRegistry.SecondNumber;
+      SumNumbers.push(DataRegistry);
+     console.log(SumNumbers);
+    var dataX = Object.assign({}, req.body);
+        dataX.digits = SumNumbers;
+          res.render('SumOfTwoNumbers', dataX);
+  });
+//------------------------------------------------------------------------------
 module.exports = router;

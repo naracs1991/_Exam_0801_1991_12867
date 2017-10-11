cb28de1ba6d684d47802940f37f47e2318c57972
naracs1991 <naracs1991@yahoo.com>
2017-10-10 19:29:07 -0800

    Question N0.16
    Commits done for part question number 16 of exam

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+  router.get('/index',
+  function(req,res,next){
+      var NameAndInfo = Object.assign('Nelson Rivera','0801_1991_12867','naracs1991@gmail.com');
+      });
+      res.json(NameAndInfo);
+  }
+ );
+//-------------------------------------------
+var numbers = {
+  firstnumber:10,
+  secondnumber:10,
+}
+
+ router.post('/SumOfTwoNumbers', function(req,res,next){
+   var firstnumber = Object.assign({},numbers,req.body);
+   var secondnumber = Object.assign({},numbers,req.body);
+   var result = (firstnumber+secondnumber);
+   res.json(result);
+  });
+
 module.exports = router;
